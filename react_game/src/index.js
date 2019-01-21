f6377e223df2683ff6a13df4338cb3747f525e19
aoife kavanagh <nyhankaa@tcd.ie>
2019-01-21 15:38:29 +0000

    replaced class Square with function Square

--- react_game/src/index.js
+++ react_game/src/index.js
@@ -10,21 +10,15 @@ import * as serviceWorker from './serviceWorker';
 // Learn more about service workers: http://bit.ly/CRA-PWA
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    render() {
-        return (
-            <button 
-                className="square" 
-                onClick={() => this.setState({value:'X'}) }
-            >
-                {this.state.value}
-            </button>
-        );
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
   }
 }
 
@@ -35,10 +29,22 @@ class Board extends React.Component {
             squares: Array(9).fill(null),
         };
     }
-      renderSquare(i) {
-        return <Square value={this.state.squares[i]} />;
-      }
     
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares:squares});
+    }
+    
+      renderSquare(i) {
+        return (
+        <Square 
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+        );
+      }
+      
       render() {
         const status = 'Next player: X';
     
